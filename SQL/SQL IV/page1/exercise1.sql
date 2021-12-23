da3f564d0a56cfe1cbb88741cc6ac831a076b8e9
andreanynthn <88206783+andreanynthn@users.noreply.github.com>
2021-12-23 21:39:43 +0700

    Delete SQL/SQL IV directory

--- SQL/SQL IV/page1/exercise1.sql
+++
@@ -1,5 +0,0 @@
--- Tambahkan data ke tabel students
-INSERT INTO students(name, course) VALUES("Kate", "Java");
-
--- Jangan menghapus kueri dibawah
-select * from students;
--- SQL/SQL IV/page3/exercise1.sql
+++
@@ -1,5 +0,0 @@
--- Hapus data yang ber-id 7 di tabel student
-DELETE FROM students
-WHERE id = 7;
--- Jangan hapus kueri dibawah
-SELECT * FROM students;
