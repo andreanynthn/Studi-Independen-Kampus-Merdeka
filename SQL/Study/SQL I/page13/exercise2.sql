9ae6aa1c264c4d16afeeed6bcf7606e6b57b306b
andreanynthn <88206783+andreanynthn@users.noreply.github.com>
2021-12-23 21:44:53 +0700

    Delete SQL/Study/SQL I/page13 directory

--- SQL/Study/SQL I/page13/exercise2.sql
+++
@@ -1,9 +0,0 @@
-/*
-dibawah WHERE character_name = "Ninja Ken",
-tambahkan code untuk menampilkan hasil maksimum 10 baris
-*/
-
-SELECT *
-FROM purchases
-WHERE character_name = "Ninja Ken"
-LIMIT 10;
