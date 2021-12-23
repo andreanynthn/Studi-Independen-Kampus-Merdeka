e17e78e4584e8ede86c0e47e05820029b0ee3366
andreanynthn <88206783+andreanynthn@users.noreply.github.com>
2021-12-23 21:45:08 +0700

    Delete SQL/Study/SQL I/page14 directory

--- SQL/Study/SQL I/page14/exercise1.sql
+++
@@ -1,8 +0,0 @@
-/*
-Dibawah "FROM purchases" tambahkan code untuk mengambil baris
-dengan nilai tanggal "2018-11-01" dan sebelumnya, dari kolom "purchased_at"
-*/
-
-SELECT *
-FROM purchases
-WHERE purchased_at <= "2018-11-01";
