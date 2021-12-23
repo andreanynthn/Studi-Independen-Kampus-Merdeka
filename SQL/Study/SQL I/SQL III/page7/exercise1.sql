cf87c8f582166ce301efb880f53472b6107a82a6
andreanynthn <88206783+andreanynthn@users.noreply.github.com>
2021-12-23 21:42:26 +0700

    Add files via upload

---
+++ SQL/Study/SQL I/SQL III/page7/exercise1.sql
@@ -0,0 +1,3 @@
+SELECT players.name, countries.name
+FROM players JOIN countries
+ON players.country_id = countries.id;
---
+++ SQL/Study/SQL I/SQL III/page8/exercise1.sql
@@ -0,0 +1,3 @@
+SELECT *
+FROM players JOIN teams
+ON players.previous_team_id = teams.id;
