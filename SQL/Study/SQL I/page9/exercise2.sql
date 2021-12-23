6a7a136397efa1c737732852c1eced1e2253f811
andreanynthn <88206783+andreanynthn@users.noreply.github.com>
2021-12-23 21:46:24 +0700

    Delete SQL/Study/SQL I/page9 directory

--- SQL/Study/SQL I/page9/exercise2.sql
+++
@@ -1,8 +0,0 @@
-/*
-dibawah "FROM purchases" tambahkan code untuk mendapatkan semua baris dimana
-kolom "name" tidak mengandung kata "puding"
-*/
-
-SELECT *
-FROM purchases
-WHERE NOT name LIKE "%puding%";
